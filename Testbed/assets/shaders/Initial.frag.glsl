0fa760579dc85de361392ac472f6923eab17a2cb
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-12-09 17:01:18 +0100

    Indexed drawing

--- Testbed/assets/shaders/Initial.frag.glsl
+++ Testbed/assets/shaders/Initial.frag.glsl
@@ -1,8 +1,8 @@
 #version 450 core
 
-out vec4 FragColor;
+out vec4 color;
 
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
--- Testbed/src/Testbed.cpp
+++ Testbed/src/Testbed.cpp
@@ -12,7 +12,7 @@ public:
     explicit TestLayer(const std::string& name)
         : SW::Layer(name) {}
 
-    u32 VBO, VAO;
+    u32 VBO, VAO, EBO;
     SW::Shader* shader;
 
     void OnAttach() override {
@@ -20,26 +20,38 @@ public:
 
         shader = new SW::Shader("assets/shaders/Initial.vert.glsl", "assets/shaders/Initial.frag.glsl");
 
-        const float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-             0.5f, -0.5f, 0.0f,
-             0.0f,  0.5f, 0.0f
+        f32 vertices[] = {
+             0.5f,  0.5f, 0.0f,  // top right
+             0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left 
+        };
+
+        u32 indices[] = {
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
         };
 
         glGenBuffers(1, &VBO);
-
         glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &EBO);
 
         glBindVertexArray(VAO);
 
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
 
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
+
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
 
     void OnDetach() override {
@@ -56,7 +68,8 @@ public:
         shader->Bind();
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     }
 
     void OnRender() override {
