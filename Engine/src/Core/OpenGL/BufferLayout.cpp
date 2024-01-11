ddb4f360f2e53ee99334bb65611dc053ec07affa
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2024-01-11 21:45:00 +0100

    Buffer layout - OpenGL renderer2d preparations

---
+++ Engine/src/Core/OpenGL/BufferLayout.cpp
@@ -0,0 +1,16 @@
+﻿#include "BufferLayout.hpp"
+
+namespace SW {
+
+	void BufferLayout::CalculateOffsetsAndStride() {
+		u32 offset = 0;
+
+		for (BufferElement& element : m_Elements) {
+			element.Offset = offset;
+			offset += element.Size;
+		}
+
+		m_Stride = offset;
+	}
+
+}
