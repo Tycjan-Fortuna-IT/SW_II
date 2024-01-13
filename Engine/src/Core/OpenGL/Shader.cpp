0c66989be5aed200c8a2dc60834d130a2ec16121
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2024-01-13 14:04:19 +0100

    Shader class optimized

--- Engine/src/Core/OpenGL/Shader.cpp
+++ Engine/src/Core/OpenGL/Shader.cpp
@@ -15,132 +15,140 @@ namespace SW {
 
     Shader::~Shader()
 	{
-        glDeleteProgram(m_ShaderHandle);
-    }
+        glDeleteProgram(m_Handle);
+	}
 
-    ShaderData Shader::ProcessShaderFiles(const std::string& vertexFilePath, const std::string& fragmentFilePath) const
-	{
-        return {
-            .vertexSourcePath = vertexFilePath,
-            .vertexSource = Utils::ReadFile(vertexFilePath),
-            .fragmentSourcePath = fragmentFilePath,
-            .fragmentSource = Utils::ReadFile(fragmentFilePath)
-        };
-    }
+	ShaderData Shader::ProcessShaderFiles(const std::string& vertexFilePath, const std::string& fragmentFilePath) const {
+		return {
+			.vertexSourcePath = vertexFilePath,
+			.vertexSource = Utils::ReadFile(vertexFilePath),
+			.fragmentSourcePath = fragmentFilePath,
+			.fragmentSource = Utils::ReadFile(fragmentFilePath)
+		};
+	}
 
-    void Shader::CompileShaders(const ShaderData& data)
-	{
-        const u32 program = glCreateProgram();
+	void Shader::CompileShaders(const ShaderData& data) {
+		const u32 program = glCreateProgram();
 
-        u32 vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		u32 vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-        const GLchar* vertexSource = data.vertexSource.c_str();
+		const GLchar* vertexSource = data.vertexSource.c_str();
 
-        glShaderSource(vertexShader, 1, &vertexSource, nullptr);
-        glCompileShader(vertexShader);
+		glShaderSource(vertexShader, 1, &vertexSource, nullptr);
+		glCompileShader(vertexShader);
 
-        SW_DEBUG("Compiling - Vertex shader: \n{}", data.vertexSource);
+		SW_DEBUG("Compiling - Vertex shader: \n{}", data.vertexSource);
 
-        i32 isVertexCompiled;
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isVertexCompiled);
+		i32 isVertexCompiled;
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isVertexCompiled);
 
-        if (!isVertexCompiled)
-        {
-            GLint maxLength = 0;
-            glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+		if (!isVertexCompiled) {
+			GLint maxLength = 0;
+			glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
 
-            std::vector<GLchar> infoLog(maxLength);
-            glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
 
-            glDeleteShader(vertexShader);
+			glDeleteShader(vertexShader);
 
-            SW_ERROR("{}", infoLog.data());
-            SW_ERROR("Vertex shader compilation failure!");
+			SW_ERROR("{}", infoLog.data());
+			SW_ERROR("Vertex shader compilation failure!");
 
-            return;
-        }
+			return;
+		}
 
 		SW_INFO("Vertex shader `{}` compiled successfully!", data.vertexSourcePath);
 
-        u32 fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		u32 fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-        const GLchar* fragmentSource = data.fragmentSource.c_str();
+		const GLchar* fragmentSource = data.fragmentSource.c_str();
 
-        glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
-        glCompileShader(fragmentShader);
+		glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
+		glCompileShader(fragmentShader);
 
 		SW_DEBUG("Compiling - Fragment shader: \n{}", data.fragmentSource);
 
-        i32 isFragmentCompiled;
-        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isFragmentCompiled);
+		i32 isFragmentCompiled;
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isFragmentCompiled);
 
-        if (!isFragmentCompiled)
-        {
-            GLint maxLength = 0;
-            glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+		if (!isFragmentCompiled) {
+			GLint maxLength = 0;
+			glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
 
-            std::vector<GLchar> infoLog(maxLength);
-            glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
 
-            glDeleteShader(vertexShader);
-            glDeleteShader(fragmentShader);
+			glDeleteShader(vertexShader);
+			glDeleteShader(fragmentShader);
 
-            SW_ERROR("{}", infoLog.data());
-            SW_ERROR("Fragment shader compilation failure!");
+			SW_ERROR("{}", infoLog.data());
+			SW_ERROR("Fragment shader compilation failure!");
 
-            return;
-        }
+			return;
+		}
 
-        SW_INFO("Fragment shader `{}` compiled successfully!", data.fragmentSourcePath);
+		SW_INFO("Fragment shader `{}` compiled successfully!", data.fragmentSourcePath);
 
-        glAttachShader(program, vertexShader);
-        glAttachShader(program, fragmentShader);
+		glAttachShader(program, vertexShader);
+		glAttachShader(program, fragmentShader);
 
-        glLinkProgram(program);
+		glLinkProgram(program);
 
-        GLint isLinked = 0;
-        glGetProgramiv(program, GL_LINK_STATUS, (int*)&isLinked);
+		GLint isLinked = 0;
+		glGetProgramiv(program, GL_LINK_STATUS, (int*)&isLinked);
 
-        if (isLinked == GL_FALSE) {
-            GLint maxLength = 0;
-            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+		if (isLinked == GL_FALSE) {
+			GLint maxLength = 0;
+			glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
 
-            std::vector<GLchar> infoLog(maxLength);
-            glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
+			std::vector<GLchar> infoLog(maxLength);
+			glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
 
-            glDeleteProgram(program);
+			glDeleteProgram(program);
 
-            glDeleteShader(vertexShader);
-            glDeleteShader(fragmentShader);
+			glDeleteShader(vertexShader);
+			glDeleteShader(fragmentShader);
 
-            SW_ERROR("Shader link failure!");
+			SW_ERROR("Shader link failure!");
 
-            return;
-        }
+			return;
+		}
 
-        glDeleteShader(vertexShader);
-        glDeleteShader(fragmentShader);
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
 
-        m_ShaderHandle = program;
-    }
+		m_Handle = program;
+	}
 
-    void Shader::Bind() const
-	{
-        glUseProgram(m_ShaderHandle);
-    }
+	void Shader::Bind() const {
+		glUseProgram(m_Handle);
+	}
 
-    void Shader::Unbind() const
-	{
-        glUseProgram(0);
-    }
+	void Shader::Unbind() const {
+		glUseProgram(0);
+	}
+
+	i32 Shader::GetUniformLocation(const std::string name) const {
+		auto location = m_UniformLocations.find(name);
+
+		if (location == m_UniformLocations.end()) {
+			i32 newLocation = glGetUniformLocation(m_Handle, name.c_str());
+
+			m_UniformLocations[name] = newLocation;
+
+			return newLocation;
+		}
+
+		return location->second;
+	}
 
     void Shader::UploadUniformInt(const std::string& name, int value) const
 	{
-        glUniform1i(glGetUniformLocation(m_ShaderHandle, name.c_str()), value);
+        glUniform1i(GetUniformLocation(name), value);
     }
 
     void Shader::UploadUniformMat4(const std::string& name, const Matrix4<f32>& value) const
 	{
-        glUniformMatrix4fv(glGetUniformLocation(m_ShaderHandle, name.c_str()), 1, GL_FALSE, value.ValuePtr());
+        glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, value.ValuePtr());
     }
 }
--- Engine/src/Core/OpenGL/Shader.hpp
+++ Engine/src/Core/OpenGL/Shader.hpp
@@ -4,34 +4,90 @@
 
 namespace SW {
 
-    struct ShaderData final
-    {
-        std::string vertexSourcePath;
-        std::string vertexSource;
-        std::string fragmentSourcePath;
-        std::string fragmentSource;
-    };
+	/**
+	 * @brief Struct representing the data required for a shader.
+	 */
+	struct ShaderData final
+	{
+		std::string vertexSourcePath;    ///< The file path of the vertex shader source code.
+		std::string vertexSource;        ///< The vertex shader source code.
+		std::string fragmentSourcePath;  ///< The file path of the fragment shader source code.
+		std::string fragmentSource;      ///< The fragment shader source code.
+	};
 
-    class Shader final
-    {
-    public:
-        Shader(const std::string& vertexFilePath, const std::string& fragmentFilePath);
-        ~Shader();
+	/**
+	 * @brief Class representing a shader.
+	 */
+	class Shader final
+	{
+	public:
+		/**
+		 * @brief Constructs a Shader object.
+		 * @param vertexFilePath The file path of the vertex shader source code.
+		 * @param fragmentFilePath The file path of the fragment shader source code.
+		 */
+		Shader(const std::string& vertexFilePath, const std::string& fragmentFilePath);
 
-        void Bind() const;
-        void Unbind() const;
+		/**
+		 * @brief Destroys the Shader object.
+		 */
+		~Shader();
 
-        const u32 GetHandle() const { return m_ShaderHandle; }
+		/**
+		 * @brief Binds the shader for rendering.
+		 */
+		void Bind() const;
 
-        void UploadUniformInt(const std::string& name, int value) const;
+		/**
+		 * @brief Unbinds the shader.
+		 */
+		void Unbind() const;
 
-        void UploadUniformMat4(const std::string& name, const Matrix4<f32>& value) const;
+		/**
+		 * @brief Gets the handle of the shader.
+		 * @return The handle of the shader.
+		 */
+		const u32 GetHandle() const;
 
-    private:
-        u32 m_ShaderHandle;
+		/**
+		 * @brief Uploads an integer uniform to the shader.
+		 * @param name The name of the uniform.
+		 * @param value The value of the uniform.
+		 */
+		void UploadUniformInt(const std::string& name, int value) const;
 
-        ShaderData ProcessShaderFiles(const std::string& vertexFilePath, const std::string& fragmentFilePath) const;
-        void CompileShaders(const ShaderData& data);
-    };
+		/**
+		 * @brief Uploads a 4x4 matrix uniform to the shader.
+		 * @param name The name of the uniform.
+		 * @param value The value of the uniform.
+		 */
+		void UploadUniformMat4(const std::string& name, const Matrix4<f32>& value) const;
+
+	private:
+		u32 m_Handle; ///< The handle of the shader.
+
+		mutable std::unordered_map<std::string, i32> m_UniformLocations; ///< Cache of uniform locations.
+
+		/**
+		 * @brief Gets the location of a uniform in the shader. (cached)
+		 * @param name The name of the uniform.
+		 * @return The location of the uniform.
+		 */
+		i32 GetUniformLocation(const std::string name) const;
+
+		/**
+		 * @brief Processes the shader files and returns the shader data.
+		 * @param vertexFilePath The file path of the vertex shader source code.
+		 * @param fragmentFilePath The file path of the fragment shader source code.
+		 * @return The shader data.
+		 */
+		ShaderData ProcessShaderFiles(const std::string& vertexFilePath, const std::string& fragmentFilePath) const;
+
+		/**
+		 * @brief Compiles the shaders using the provided shader data.
+		 * @param data The shader data.
+		 */
+		void CompileShaders(const ShaderData& data);
+	};
 
 }
