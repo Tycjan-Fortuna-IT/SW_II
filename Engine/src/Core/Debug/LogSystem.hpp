d55113ab7963e9d1dec7f351e60b5da4a0610ef0
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2024-03-10 18:17:09 +0100

    Log system finally upgraded to more convenient and usefull, especially for ASSERT macro

--- Engine/src/Core/Debug/LogSystem.hpp
+++ Engine/src/Core/Debug/LogSystem.hpp
@@ -1,8 +1,8 @@
 /**
  * @file LogSystem.hpp
  * @author Tycjan Fortuna (242213@edu.p.lodz.pl)
- * @version 0.1.1
- * @date 2024-01-02
+ * @version 0.2.0
+ * @date 2024-03-10
  * 
  * @copyright Copyright (c) 2024 Tycjan Fortuna
  */
@@ -19,36 +19,33 @@ namespace SW {
 	 * in the logging system. Each log type has a specific meaning and is used
 	 * to categorize log messages.
 	 */
-	enum LogType : u8
-    {
-        ENGINE, /**< Log type for engine-related messages. */
-        APP,	/**< Log type for application-specific messages. */
-    };
+	enum LogType : u8 {
+		ENGINE, /**< Log type for engine-related messages. */
+		APP,	/**< Log type for application-specific messages. */
+	};
 
 	/**
 	 * @brief Enumeration defining different log levels.
-	 * 
+	 *
 	 * This enumeration represents the possible log levels that can be used
 	 * in the logging system. Each log level has a specific meaning and is used
 	 * to categorize log messages by their severity.
 	 */
-    enum LogLevel : u8
-    {
-        LOG_LEVEL_TRACE, /**< Log level for trace messages. */
-        LOG_LEVEL_INFO,  /**< Log level for informational messages. */
-        LOG_LEVEL_WARN,  /**< Log level for warning messages. */
-        LOG_LEVEL_DEBUG, /**< Log level for debug messages. */
-        LOG_LEVEL_ERROR, /**< Log level for error messages. */
-        LOG_LEVEL_FATAL, /**< Log level for fatal messages. */
-    };
+	enum LogLevel : u8 {
+		LOG_LEVEL_TRACE, /**< Log level for trace messages. */
+		LOG_LEVEL_INFO,  /**< Log level for informational messages. */
+		LOG_LEVEL_WARN,  /**< Log level for warning messages. */
+		LOG_LEVEL_DEBUG, /**< Log level for debug messages. */
+		LOG_LEVEL_ERROR, /**< Log level for error messages. */
+		LOG_LEVEL_FATAL, /**< Log level for fatal messages. */
+	};
 
 	/**
 	 * @brief The LogSystem class is responsible for logging messages to the console.
 	 *        It is a static class and should not be instantiated.
 	 */
-    class LogSystem final
-    {
-    public:
+	class LogSystem final {
+	public:
 		/**
 		 * @brief Initializes the log system.
 		 * @warning This method should be called before any logging is done!!
@@ -66,30 +63,33 @@ namespace SW {
 		 * @brief Prints using spdlog logger logs to specified sinks.
 		 * 		  Type and severity of the log is specified by the parameters.
 		 * @note  This method should not be used directly, but rather through the FATAL, ERROR, WARN, INFO, DEBUG, and TRACE macros.
-		 * 
+		 *
 		 * @param type The log type to set the log level for.
 		 * @param level The log severity level.
-		 * @param message The message to log. Must be a fmt formattable string literal. e.g. "Hello, {}!"
+		 * @param tag The tag to display before formatted message.
+		 * @param format The message to log. Must be a string literal.
 		 * @param args The arguments to the message to be formatted.
 		 */
 		template <typename... Args>
-        static void PrintMessage(LogType type, LogLevel level, const std::string& message, Args&&... args)
+		static void PrintMessage(LogType type, LogLevel level, std::string_view tag, std::string_view format, Args&&... args)
 		{
 			const std::shared_ptr<spdlog::logger>& logger = type == LogType::ENGINE ? s_EngineLogger : s_AppLogger;
 
+			std::string message = fmt::vformat(format, fmt::make_format_args(std::forward<Args>(args)...));
+
 			switch (level) {
 				case LOG_LEVEL_TRACE:
-					logger->trace(fmt::vformat(message, fmt::make_format_args(std::forward<Args>(args)...))); break;
+					logger->trace("{0}{1}", tag, message); break;
 				case LOG_LEVEL_INFO:
-					logger->info(fmt::vformat(message, fmt::make_format_args(std::forward<Args>(args)...))); break;
+					logger->info("{0}{1}", tag, message); break;
 				case LOG_LEVEL_WARN:
-					logger->warn(fmt::vformat(message, fmt::make_format_args(std::forward<Args>(args)...))); break;
+					logger->warn("{0}{1}", tag, message); break;
 				case LOG_LEVEL_DEBUG:
-					logger->debug(fmt::vformat(message, fmt::make_format_args(std::forward<Args>(args)...))); break;
+					logger->debug("{0}{1}", tag, message); break;
 				case LOG_LEVEL_ERROR:
-					logger->error(fmt::vformat(message, fmt::make_format_args(std::forward<Args>(args)...))); break;
+					logger->error("{0}{1}", tag, message); break;
 				case LOG_LEVEL_FATAL:
-					logger->critical(fmt::vformat(message, fmt::make_format_args(std::forward<Args>(args)...)));
+					logger->critical("{0}{1}", tag, message);
 					break;
 			}
 		}
@@ -97,15 +97,15 @@ namespace SW {
 		/**
 		 * @brief Reports an assertion failure.
 		 * @note  This method should not be used directly, but rather through the ASSERT macro.
-		 * 
+		 *
 		 * @param expression String representation of the expression that failed.
 		 * @param message The message to log.
 		 * @param file File in which the assertion failed.
 		 * @param line Line at which the assertion failed.
 		 */
-        static void ReportAssertionFailure(const char* expression, const char* message, const char* file, i16 line);
+		static void ReportAssertionFailure(const char* expression, const char* message, const char* file, i16 line);
 
-    private:
+	private:
 		/**
 		 * @brief Logger for engine related logs.
 		 */
@@ -115,170 +115,183 @@ namespace SW {
 		 * @brief Logger for client's application related logs.
 		 */
 		static std::shared_ptr<spdlog::logger> s_AppLogger;
-    };
+	};
 
-#ifndef SW_DISABLE_LOGS
-    /**
-     * @brief Asserts that an expression is true. (Engine use only.)
-     *        If the expression is false, the application will be halted.
-     * @param x The expression to assert.
-     * @param msg The message to print if the assertion fails.
-     */
-    #define ASSERT(x, msg)															 \
-    {																				 \
-        if (!(x))																	 \
-        {																			 \
-            ::SW::LogSystem::ReportAssertionFailure(#x, msg, __FILE__, __LINE__);    \
-            DEBUG_BREAK();															 \
-        }																			 \
+#ifndef SW_DISABLE_ASSERTS
+
+	/**
+	 * @brief Asserts that an expression is true. (Engine use only.)
+	 *        If the expression is false, the application will be halted.
+	 * @param x The expression to assert.
+	 * @param msg The message to print if the assertion fails.
+	 */
+	#define ASSERT(x, ...)																									\
+    {																														\
+        if (!(x))																											\
+        {																													\
+            ::SW::LogSystem::PrintMessage(																					\
+				::SW::LogType::ENGINE, ::SW::LogLevel::LOG_LEVEL_FATAL, "Assertion failed:  " __VA_OPT__(, ) ##__VA_ARGS__  \
+			);																												\
+            DEBUG_BREAK();																									\
+        }																													\
     }
 
-    /**
-     * @brief Logs a message.
-     * @note This macro should not be used directly, but rather through the FATAL, ERROR, WARN, INFO, DEBUG, and TRACE macros.
-     *
-     * @param level The level of the message.
-     * @param msg The message to log.
-     * @param ... The arguments to the message to be formatted.
-     */
-    #define SW_LOG(type, level, msg, ...) ::SW::LogSystem::PrintMessage(type, level, msg, ##__VA_ARGS__)
+#else
 
-    /**
-     * @brief Logs a fatal message.
-     *        This macro should be used to stop the application when hit.
-     * @note Engine use only.
-	 * 
-     * @param msg The message to log.
-     * @param ... The arguments to the message to be formatted.
-     */
-    #define SW_FATAL(msg, ...) { SW_LOG(ENGINE, LOG_LEVEL_FATAL, msg, ##__VA_ARGS__); DEBUG_BREAK(); }
+	#define ASSERT(x, ...)
 
-    /**
-     * @brief Logs an error message.
-     *        This macro should be used to indicate critical runtime problems that cause the application to run improperly or not at all.
-     * @note Engine use only.
-	 * 
-     * @param msg The message to log.
-     * @param ... The arguments to the message to be formatted.
-     */
-    #define SW_ERROR(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_ERROR, msg, ##__VA_ARGS__)
+#endif // SW_DISABLE_ASSERTS
 
-    /**
-     * @brief Logs a warning message.
-     *        This macro should be used to indicate non-critial problems with the application that cause it to run suboptimally.
-     * @note Engine use only.
-	 * 
-     * @param msg The message to log.
-     * @param ... The arguments to the message to be formatted.
-     */
-    #define SW_WARN(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_WARN, msg, ##__VA_ARGS__)
+#ifndef SW_DISABLE_LOGS
 
-    /**
-     * @brief Logs an info message.
-     *        This macro should be used for non-erronuous informational purposes.
+	/**
+	 * @brief Logs a message.
+	 * @note This macro should not be used directly, but rather through the FATAL, ERROR, WARN, INFO, DEBUG, and TRACE macros.
+	 *
+	 * @param level The level of the message.
+	 * @param msg The message to log.
+	 * @param ... The arguments to the message to be formatted.
+	 */
+	#define SW_LOG(type, level, msg, ...) ::SW::LogSystem::PrintMessage(::SW::LogType::type, ::SW::LogLevel::level, "", msg __VA_OPT__(, ) ##__VA_ARGS__)
+
+	/**
+	 * @brief Logs a trace message.
+	 *        This macro should be used for verbose debugging purposes.
 	 * @note Engine use only.
-     *
-     * @param msg The message to log.
-     * @param ... The arguments to the message to be formatted.
-     */
-    #define SW_INFO(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_INFO, msg, ##__VA_ARGS__)
-
-    /**
-     * @brief Logs a debug message.
-     *        This macro should be used for debugging purposes.
-	 * @note Engine use only.
-     *
-     * @param msg The message to log.
-     * @param ... The arguments to the message to be formatted.
-     */
-    #define SW_DEBUG(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_DEBUG, msg, ##__VA_ARGS__)
-
-    /**
-     * @brief Logs a trace message.
-     *        This macro should be used for verbose debugging purposes.
-     * @note Engine use only.
-	 * 
-     * @param msg The message to log.
-     * @param ... The arguments to the message to be formatted.
-     */
-    #define SW_TRACE(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_TRACE, msg, ##__VA_ARGS__)
-
-	/**
-	 * @brief Logs a fatal message.
-	 * 	  	  This macro should be used to stop the application when hit.
-	 * @note Application use only.
-	 * 
+	 *
 	 * @param msg The message to log.
 	 * @param ... The arguments to the message to be formatted.
 	 */
-	#define APP_FATAL(msg, ...) { SW_LOG(::SW::APP, ::SW::LOG_LEVEL_FATAL, msg, ##__VA_ARGS__); DEBUG_BREAK(); }
-
-	/**
-	 * @brief Logs an error message.
-	 * 	  	  This macro should be used to indicate critical runtime problems that cause the application to run improperly or not at all.
-	 * @note Application use only.
-	 * 
-	 * @param msg The message to log.
-	 * @param ... The arguments to the message to be formatted.
-	 */
-	#define APP_ERROR(msg, ...) SW_LOG(::SW::APP, ::SW::LOG_LEVEL_ERROR, msg, ##__VA_ARGS__)
-
-	/**
-	 * @brief Logs a warning message.
-	 * 	  	  This macro should be used to indicate non-critial problems with the application that cause it to run suboptimally.
-	 * @note Application use only.
-	 * 
-	 * @param msg The message to log.
-	 * @param ... The arguments to the message to be formatted.
-	 */
-	#define APP_WARN(msg, ...) SW_LOG(::SW::APP, ::SW::LOG_LEVEL_WARN, msg, ##__VA_ARGS__)
+	#define SW_TRACE(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_TRACE, msg, ##__VA_ARGS__)
 
 	/**
 	 * @brief Logs an info message.
-	 * 	  	  This macro should be used for non-erronuous informational purposes.
-	 * @note Application use only.
-	 * 
+	 *        This macro should be used for non-erronuous informational purposes.
+	 * @note Engine use only.
+	 *
 	 * @param msg The message to log.
 	 * @param ... The arguments to the message to be formatted.
 	 */
-	#define APP_INFO(msg, ...) SW_LOG(::SW::APP, ::SW::LOG_LEVEL_INFO, msg, ##__VA_ARGS__)
+	#define SW_INFO(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_INFO, msg, ##__VA_ARGS__)
 
 	/**
 	 * @brief Logs a debug message.
-	 * 	  	  This macro should be used for debugging purposes.
-	 * @note Application use only.
-	 * 
+	 *        This macro should be used for debugging purposes.
+	 * @note Engine use only.
+	 *
 	 * @param msg The message to log.
 	 * @param ... The arguments to the message to be formatted.
 	 */
-	#define APP_DEBUG(msg, ...) SW_LOG(::SW::APP, ::SW::LOG_LEVEL_DEBUG, msg, ##__VA_ARGS__)
+	#define SW_DEBUG(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_DEBUG, msg, ##__VA_ARGS__)
+
+	/**
+	 * @brief Logs a warning message.
+	 *        This macro should be used to indicate non-critial problems with the application that cause it to run suboptimally.
+	 * @note Engine use only.
+	 *
+	 * @param msg The message to log.
+ 	 * @param ... The arguments to the message to be formatted.
+ 	 */
+	#define SW_WARN(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_WARN, msg, ##__VA_ARGS__)
+
+	/**
+	 * @brief Logs an error message.
+	 *        This macro should be used to indicate critical runtime problems that cause the application to run improperly or not at all.
+	 * @note Engine use only.
+	 *
+	 * @param msg The message to log.
+	 * @param ... The arguments to the message to be formatted.
+	 */
+	#define SW_ERROR(msg, ...) SW_LOG(ENGINE, LOG_LEVEL_ERROR, msg, ##__VA_ARGS__)
+
+	/**
+	 * @brief Logs a fatal message.
+	 *        This macro should be used to stop the application when hit.
+	 * @note Engine use only.
+	 *
+	 * @param msg The message to log.
+	 * @param ... The arguments to the message to be formatted.
+	 */
+	#define SW_FATAL(msg, ...) { SW_LOG(ENGINE, LOG_LEVEL_FATAL, msg, ##__VA_ARGS__); DEBUG_BREAK(); }
 
 	/**
 	 * @brief Logs a trace message.
 	 * 	  	  This macro should be used for verbose debugging purposes.
 	 * @note Application use only.
-	 * 
+	 *
 	 * @param msg The message to log.
 	 * @param ... The arguments to the message to be formatted.
 	 */
-	#define APP_TRACE(msg, ...) SW_LOG(::SW::APP, ::SW::LOG_LEVEL_TRACE, msg, ##__VA_ARGS__)
-}
+	#define APP_TRACE(msg, ...) SW_LOG(APP, LOG_LEVEL_TRACE, msg, ##__VA_ARGS__)
+
+	/**
+	 * @brief Logs an info message.
+	 * 	  	  This macro should be used for non-erronuous informational purposes.
+	 * @note Application use only.
+	 *
+	 * @param msg The message to log.
+	 * @param ... The arguments to the message to be formatted.
+	 */
+	#define APP_INFO(msg, ...) SW_LOG(APP, LOG_LEVEL_INFO, msg, ##__VA_ARGS__)
+
+	/**
+	 * @brief Logs a debug message.
+	 * 	  	  This macro should be used for debugging purposes.
+	 * @note Application use only.
+	 *
+	 * @param msg The message to log.
+	 * @param ... The arguments to the message to be formatted.
+	 */
+	#define APP_DEBUG(msg, ...) SW_LOG(APP, LOG_LEVEL_DEBUG, msg, ##__VA_ARGS__)
+
+	/**
+	 * @brief Logs a warning message.
+	 * 	  	  This macro should be used to indicate non-critial problems with the application that cause it to run suboptimally.
+	 * @note Application use only.
+	 *
+	 * @param msg The message to log.
+	 * @param ... The arguments to the message to be formatted.
+	 */
+	#define APP_WARN(msg, ...) SW_LOG(APP, LOG_LEVEL_WARN, msg, ##__VA_ARGS__)
+
+	/**
+	 * @brief Logs an error message.
+	 * 	  	  This macro should be used to indicate critical runtime problems that cause the application to run improperly or not at all.
+	 * @note Application use only.
+	 *
+	 * @param msg The message to log.
+	 * @param ... The arguments to the message to be formatted.
+	 */
+	#define APP_ERROR(msg, ...) SW_LOG(APP, LOG_LEVEL_ERROR, msg, ##__VA_ARGS__)
+
+	/**
+	 * @brief Logs a fatal message.
+	 * 	  	  This macro should be used to stop the application when hit.
+	 * @note Application use only.
+	 *
+	 * @param msg The message to log.
+	 * @param ... The arguments to the message to be formatted.
+	 */
+	#define APP_FATAL(msg, ...) { SW_LOG(APP, LOG_LEVEL_FATAL, msg, ##__VA_ARGS__); DEBUG_BREAK(); }
+
 #else
 
-	#define ASSERT(x, msg)
-
 	#define SW_LOG(type, level, msg, ...)
-	#define SW_FATAL(msg, ...)
-	#define SW_WARN(msg, ...)
+	#define SW_TRACE(msg, ...)
 	#define SW_INFO(msg, ...)
 	#define SW_DEBUG(msg, ...)
-	#define SW_TRACE(msg, ...)
+	#define SW_WARN(msg, ...)
+	#define SW_ERROR(msg, ...)
+	#define SW_FATAL(msg, ...)
 
 	#define APP_LOG(type, level, msg, ...)
-	#define APP_FATAL(msg, ...)
-	#define APP_WARN(msg, ...)
+	#define APP_TRACE(msg, ...)
 	#define APP_INFO(msg, ...)
 	#define APP_DEBUG(msg, ...)
-	#define APP_TRACE(msg, ...)
+	#define APP_WARN(msg, ...)
+	#define APP_ERROR(msg, ...)
+	#define APP_FATAL(msg, ...)
 
-#endif // !DISABLE_LOGS
+#endif // SW_DISABLE_LOGS
+
+}
