29f7b6120a4cabb42d531d08aa9db18d54b122b3
Tycjan Fortuna <242213@edu.p.lodz.pl>
2024-05-02 22:12:38 +0200

    Upgraded spritesheet editor, more user friendly with some QOL changes like auto sprite generatation, better visuals and grid coord labels

--- EngineEditor/src/AssetPanels/SpritesheetEditor.cpp
+++ EngineEditor/src/AssetPanels/SpritesheetEditor.cpp
@@ -5,29 +5,21 @@
 #include "GUI/Editor/EditorResources.hpp"
 #include "Asset/AssetLoader.hpp"
 #include "Core/Project/ProjectContext.hpp"
-#include "GUI/GUI_V2.hpp"
+#include "GUI/GUI.hpp"
 #include "Core/Utils/SerializationUtils.hpp"
+#include "GUI/Appearance.hpp"
 
 namespace SW {
+
+	constexpr f32 MIN_ZOOM = 0.1f;
+	constexpr f32 MAX_ZOOM = 15.0f;
+	constexpr f32 MIN_GRID_SIZE = 16.f;
+	constexpr f32 MAX_GRID_SIZE = 4000.f;
 
 	SpritesheetEditor::SpritesheetEditor(const char* name, const char* icon)
 		: AssetEditorPanel(name, icon)
 	{
-		EventSystem::Register(EVENT_CODE_MOUSE_WHEEL, nullptr, [this](Event event, void* sender, void* listener) -> bool {
-			const f32 yOffset = event.Payload.f32[1];
 
-			if (!m_IsCanvasHovered)
-				return false;
-
-			if (m_Spritesheet) {
-				const f32 offset = yOffset / 10.f;
-				if ((*m_Spritesheet)->ViewZoom + offset > 0.1f && (*m_Spritesheet)->ViewZoom + offset < 15.f)
-					(*m_Spritesheet)->ViewZoom += offset;
-			}
-				
-
-			return false;
-		});
 	}
 
 	void SpritesheetEditor::OnUpdate(Timestep ts)
@@ -52,9 +44,18 @@ namespace SW {
 
 	void SpritesheetEditor::OnClose()
 	{
-		//const AssetMetaData& metadata = AssetManager::GetAssetMetaData((*m_Spritesheet)->GetHandle());
-		
-		//AssetLoader::Serialize(metadata);
+
+	}
+
+	// TODO move to GUI.hpp
+	static bool BeginHeaderCollapse(
+		const char* name, ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_None
+	) {
+		return ImGui::TreeNodeEx(name, flags | ImGuiTreeNodeFlags_AllowItemOverlap | ImGuiTreeNodeFlags_Framed, "%s", name);
+	}
+
+	static void EndHeaderCollapse() {
+		ImGui::TreePop();
 	}
 
 	void SpritesheetEditor::Render()
@@ -63,6 +64,7 @@ namespace SW {
 
 		ImVec2 viewOrigin = { vo.x, vo.y };
 		m_Canvas.SetView(viewOrigin, (*m_Spritesheet)->ViewZoom);
+		(*m_Spritesheet)->ViewPos = glm::vec2{ viewOrigin.x, viewOrigin.y };
 
 		ImGuiIO& io = ImGui::GetIO();
 
@@ -74,32 +76,57 @@ namespace SW {
         constexpr ImGuiTableFlags flags = ImGuiTableFlags_SizingFixedFit
             | ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders | ImGuiTableFlags_Resizable;
 
-		f32 wz = (*m_Spritesheet)->ViewZoom;
+		ImGui::PushFont(GUI::Appearance::GetFonts().BigBoldFont);
+		ImGui::Text(
+			"Currently editing: %s [%i x %i]",
+			m_Name.c_str(), 
+			(*m_Spritesheet)->GetSpritesheetTexture()->GetWidth(), 
+			(*m_Spritesheet)->GetSpritesheetTexture()->GetHeight()
+		);
+		ImGui::PopFont();
+
+		GenerateFromGridPopup(&m_OpenGenerateFromGridModal);
 
 		if (ImGui::BeginTable("MainViewTable", 2, flags, ImGui::GetContentRegionAvail())) {
 			ImGui::TableNextRow();
 			ImGui::TableNextColumn();
 
-			GUI2::Properties::BeginProperties("##spritesheet_editor_properties");
+			ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, 0.f);
+			if (BeginHeaderCollapse("General Options", ImGuiTreeNodeFlags_DefaultOpen)) {
+				GUI::Properties::BeginProperties("##spritesheet_editor_general_options");
 
-			GUI2::Properties::ScalarSliderProperty(&(*m_Spritesheet)->ViewZoom, "View Zoom", nullptr, 0.1f, 15.0f);
-			GUI2::Properties::ScalarSliderProperty(&(*m_Spritesheet)->GridSize, "Grid Size", nullptr, 16.f, 4000.f);
-			GUI2::Properties::Vector2InputProperty(&(*m_Spritesheet)->CenterOffset, "Center Offset");
-			GUI2::Properties::CheckboxProperty(&(*m_Spritesheet)->ShowImageBorders, "Show Image Borders");
-			GUI2::Properties::DrawFolderPickerProperty(&(*m_Spritesheet)->ExportPath, "Export Path");
+				GUI::Properties::ScalarSliderProperty(&(*m_Spritesheet)->GridSize, "Grid Size", nullptr, MIN_GRID_SIZE, MAX_GRID_SIZE);
+				GUI::Properties::Vector2InputProperty(&(*m_Spritesheet)->CenterOffset, "Center Offset");
+				GUI::Properties::DrawFolderPickerProperty(&(*m_Spritesheet)->ExportPath, "Export Path");
 
-			AssetHandle handle = (*m_Spritesheet)->GetSpritesheetTexture() ? (*m_Spritesheet)->GetSpritesheetTexture()->GetHandle() : 0u;
-			if (GUI2::Properties::AssetSearchProperty<Texture2D>(&handle, "Spritesheet")) {
-				Texture2D** texture = handle ? AssetManager::GetAssetRaw<Texture2D>(handle) : nullptr;
+				AssetHandle handle = (*m_Spritesheet)->GetSpritesheetTexture() ? (*m_Spritesheet)->GetSpritesheetTexture()->GetHandle() : 0u;
+				if (GUI::Properties::AssetSearchProperty<Texture2D>(&handle, "Spritesheet")) {
+					Texture2D** texture = handle ? AssetManager::GetAssetRaw<Texture2D>(handle) : nullptr;
 
-				(*m_Spritesheet)->SetSpritesheetTexture(texture);
+					(*m_Spritesheet)->SetSpritesheetTexture(texture);
+				}
+
+				GUI::Properties::EndProperties();
+
+				EndHeaderCollapse();
 			}
 
-			(*m_Spritesheet)->ViewPos = glm::vec2{ viewOrigin.x, viewOrigin.y };
+			if (BeginHeaderCollapse("Advanced Options")) {
+				GUI::Properties::BeginProperties("##spritesheet_editor_advanced_options");
 
-			GUI2::Properties::EndProperties();
+				if (GUI::Properties::ScalarSliderProperty(&(*m_Spritesheet)->ViewZoom, "View Zoom", nullptr, MIN_ZOOM, MAX_ZOOM)) {
+					m_Canvas.SetView(viewOrigin, (*m_Spritesheet)->ViewZoom);
+				}
+				GUI::Properties::CheckboxProperty(&(*m_Spritesheet)->ShowImageBorders, "Show Image Borders");
+				GUI::Properties::CheckboxProperty(&(*m_Spritesheet)->ShowGridCoords, "Show Grid Coordinates");
 
-			//RenderSpriteCards((*m_Spritesheet)->GridSize);
+				GUI::Properties::EndProperties();
+
+				EndHeaderCollapse();
+			}
+			ImGui::PopStyleVar();
+
+			RenderSpriteCards((*m_Spritesheet)->GridSize);
 
 			ImGui::TableNextColumn();
 
@@ -115,6 +142,23 @@ namespace SW {
 					(*m_Spritesheet)->ViewPos = glm::vec2{ nvp.x, nvp.y };
 				} else if (isDragging) {
 					isDragging = false;
+				} else if (!isDragging && ImGui::IsItemHovered() && io.MouseWheel) {
+					// proper zooming finally
+					const ImVec2 mousePos = io.MousePos;
+
+					const ImGuiEx::CanvasView oldView = m_Canvas.View();
+					const f32 newViewScale = glm::clamp(m_Canvas.ViewScale() * powf(1.1f, io.MouseWheel), MIN_ZOOM, MAX_ZOOM);
+
+					m_Canvas.SetView(viewOrigin, newViewScale);
+					const ImGuiEx::CanvasView newView = m_Canvas.View();
+
+					const ImVec2 screenPosition = m_Canvas.FromLocal(mousePos, oldView);
+					const ImVec2 canvasPosition = m_Canvas.ToLocal(screenPosition, newView);
+					const ImVec2 originOffset = (canvasPosition - mousePos) * newViewScale;
+
+					m_Canvas.SetView(viewOrigin + originOffset, newViewScale);
+					(*m_Spritesheet)->ViewZoom = newViewScale;
+					(*m_Spritesheet)->ViewPos = glm::vec2{ viewOrigin.x + originOffset.x, viewOrigin.y + originOffset.y };
 				}
 
 				ImRect viewRect = m_Canvas.ViewRect();
@@ -122,7 +166,7 @@ namespace SW {
 				glm::vec2 offset = (*m_Spritesheet)->CenterOffset;
 				const ImVec2 lineOffset = { offset.x, offset.y };
 
-				ImVec2 loffset = m_Canvas.ViewOrigin() * (1.0f / wz) + lineOffset;
+				ImVec2 loffset = m_Canvas.ViewOrigin() * (1.0f / (*m_Spritesheet)->ViewZoom) + lineOffset;
 				ImVec2 viewPos = m_Canvas.ViewRect().Min + lineOffset;
 				ImVec2 viewSize = m_Canvas.ViewRect().GetSize();
 				ImDrawList* drawList = ImGui::GetWindowDrawList();
@@ -134,18 +178,6 @@ namespace SW {
 					drawList->AddRectFilled(rect.Min, rect.Max, GUI::Theme::BackgroundDark);
 				}
 
-				for (f32 x = fmodf(loffset.x, gridScale); x < viewSize.x; x += gridScale) {
-					if (x < 0) continue;
-				
-					drawList->AddLine(ImVec2(x - offset.x, 0.0f - offset.y) + viewPos, ImVec2(x - offset.x, viewSize.y - offset.y) + viewPos, GUI::Theme::TextBrighter);
-				}
-
-				for (f32 y = fmodf(loffset.y, gridScale); y < viewSize.y; y += gridScale) {
-					if (y < 0) continue;
-
-					drawList->AddLine(ImVec2(0.0f - offset.x, y - offset.y) + viewPos, ImVec2(viewSize.x - offset.x, y - offset.y) + viewPos, GUI::Theme::TextBrighter);
-				}
-
 				Texture2D* spritesheetTexture = (*m_Spritesheet)->GetSpritesheetTexture();
 				if (!spritesheetTexture)
 					spritesheetTexture = EditorResources::MissingAssetIcon;
@@ -153,16 +185,56 @@ namespace SW {
 				const ImVec2 texSize = { (f32)spritesheetTexture->GetWidth(), (f32)spritesheetTexture->GetHeight() };
 
 				ImGui::Image(
-					GUI2::GetTextureID(spritesheetTexture->GetTexHandle()), texSize,
+					GUI::GetTextureID(spritesheetTexture->GetTexHandle()), texSize,
 					{ 0, 1 }, { 1, 0 }
 				);
 
+				// in the middle, a dot
+				drawList->AddCircleFilled(
+					ImVec2(0.f, 0.f),
+					2.0f, GUI::Theme::MissingMesh
+				);
+
+				for (f32 x = fmodf(loffset.x, gridScale); x < viewSize.x; x += gridScale) {
+					if (x < 0) continue;
+
+					drawList->AddLine(
+						ImVec2(x - offset.x - 0.5f, 0.0f - offset.y) + viewPos,
+						ImVec2(x - offset.x - 0.5f, viewSize.y - offset.y) + viewPos, GUI::Theme::Selection,
+						2.f
+					);
+
+					if ((*m_Spritesheet)->ShowGridCoords) {
+						for (f32 y = fmodf(loffset.y, gridScale); y < viewSize.y; y += gridScale) {
+							if (y < 0) continue;
+
+							const int gridX = (int)std::round(x - loffset.x);
+							const int gridY = (int)std::round(y - loffset.y);
+
+							drawList->AddText(
+								ImVec2(x - offset.x - 0.5f, y - offset.y - 0.5f) + viewPos,
+								GUI::Theme::TextBrighter,
+								(std::to_string(gridX) + ", " + std::to_string(gridY)).c_str()
+							);
+						}
+					}
+				}
+
+				for (f32 y = fmodf(loffset.y, gridScale); y < viewSize.y; y += gridScale) {
+					if (y < 0) continue;
+
+					drawList->AddLine(
+						ImVec2(0.0f - offset.x, y - offset.y - 0.5f) + viewPos,
+						ImVec2(viewSize.x - offset.x, y - offset.y - 0.5f) + viewPos, GUI::Theme::Selection,
+						2.f
+					);
+				}
 
 				if ((*m_Spritesheet)->ShowImageBorders) {
-					DrawRectOnCanvas(drawList, glm::vec2{ -1.f, -1.f }, glm::vec2{ texSize.x + 2.f, texSize.y + 2.f }, GUI::Theme::NiceBlue);
+					DrawRectOnCanvas(drawList, glm::vec2{ -1.f, -1.f }, glm::vec2{ texSize.x + 1.f, texSize.y + 1.f }, GUI::Theme::NiceBlue);
 
 					for (const SpriteData& sprite : (*m_Spritesheet)->Sprites) {
-						DrawRectOnCanvas(drawList, sprite.Position - 1.f, sprite.Size + 2.f, GUI::Theme::InvalidPrefab);
+						DrawRectOnCanvas(drawList, sprite.Position, sprite.Size - 1.f, GUI::Theme::InvalidPrefab);
 					}
 				}
 
@@ -178,6 +250,9 @@ namespace SW {
 	void SpritesheetEditor::SetAssetHandle(AssetHandle handle)
 	{
 		m_Spritesheet = AssetManager::GetAssetRaw<Spritesheet>(handle);
+
+		AssetMetaData metadata = AssetManager::GetAssetMetaData(handle);
+		m_Name = metadata.Path.filename().string();
 	}
 
 	void SpritesheetEditor::DrawRectOnCanvas(ImDrawList* drawList, const glm::vec2& position, const glm::vec2& size, u32 color) const
@@ -212,28 +287,42 @@ namespace SW {
 
 		ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 16.f);
 
-		if (ImGui::Button(SW_ICON_PLUS "Add"))
+		if (ImGui::Button(SW_ICON_PLUS " Add"))
 			AddNewSprite();
 		
 		ImGui::SameLine();
 
-		if (ImGui::Button(SW_ICON_SAVE "Save")) {
+		if (ImGui::Button(SW_ICON_SAVE " Save")) {
 			const AssetMetaData& metadata = AssetManager::GetAssetMetaData((*m_Spritesheet)->GetHandle());
 
 			AssetLoader::Serialize(metadata);
+		}
+
+		// generate from grid
+		if ((*m_Spritesheet)->GetSpritesheetTexture()) {
+			ImGui::SameLine();
+
+			if (ImGui::Button(SW_ICON_GRID " Generate from grid"))
+				m_OpenGenerateFromGridModal = true;
 		}
 		
 		if (!(*m_Spritesheet)->ExportPath.empty() && (*m_Spritesheet)->GetSpritesheetTexture()) {
 			ImGui::SameLine();
 		
-			if (ImGui::Button(SW_ICON_EXPORT "Export")) {
+			if (ImGui::Button(SW_ICON_EXPORT " Export")) {
 				ExportSprites();
 			}
 		}
 
+		ImGui::SameLine();
+
+		if (ImGui::Button(SW_ICON_DELETE " Clear")) {
+			(*m_Spritesheet)->Sprites.clear();
+		}
+
 		const f32 availableWidth = ImGui::GetContentRegionAvail().x - 16.f;
 
-		GUI2::Widgets::SearchInput(&m_SearchString);
+		GUI::Widgets::SearchInput(&m_SearchString);
 
 		if (ImGui::BeginTable("SideViewTable", 1, flags, ImGui::GetContentRegionAvail())) {
 			ImGui::TableNextRow();
@@ -242,7 +331,7 @@ namespace SW {
 			int indexToRemove = -1;
 
 			if (!(*m_Spritesheet)->GetSpritesheetTexture()) {
-				GUI2::MoveMousePosX(16.f);
+				GUI::MoveMousePosX(16.f);
 				ImGui::TextUnformatted("Choose texture to proceed!");
 				ImGui::EndTable();
 				return;
@@ -254,8 +343,10 @@ namespace SW {
 
 				SpriteData& sprite = sprites[i];
 
-				if (!m_SearchString.empty() && sprite.Name.find(m_SearchString))
+				if (!m_SearchString.empty() && sprite.Name.find(m_SearchString)) {
+					ImGui::PopID();
 					continue;
+				}
 
 				ImGui::TableNextRow();
 				ImGui::TableNextColumn();
@@ -289,18 +380,18 @@ namespace SW {
 				}
 
 				if (opened) {
-					GUI2::Properties::BeginProperties(uidc);
+					GUI::Properties::BeginProperties(uidc);
 
-					GUI2::Properties::SingleLineTextInputProperty<128>(&sprite.Name, "Sprite name");
-					GUI2::Properties::Vector2InputProperty(&sprite.Position, "Position", nullptr, 0.f, 0.f, FLT_MAX, "%1.f");
-					GUI2::Properties::Vector2InputProperty(&sprite.Size, "Size", nullptr, (*m_Spritesheet)->GridSize, 1.f, 1024.f, "%1.f");
+					GUI::Properties::SingleLineTextInputProperty<128>(&sprite.Name, "Sprite name");
+					GUI::Properties::Vector2InputProperty(&sprite.Position, "Position", nullptr, 0.f, 0.f, FLT_MAX, "%1.f");
+					GUI::Properties::Vector2InputProperty(&sprite.Size, "Size", nullptr, (*m_Spritesheet)->GridSize, 1.f, 1024.f, "%1.f");
 					glm::vec2 position = sprite.Position + (*m_Spritesheet)->CenterOffset;
-					GUI2::Properties::DrawImagePartProperty(
+					GUI::Properties::DrawImagePartProperty(
 						(*m_Spritesheet)->GetSpritesheetTexture(), "Sprite", nullptr, position, sprite.Size, glm::vec4(1.0f),
 						(*m_Spritesheet)->ShowImageBorders
 					);
 
-					GUI2::Properties::EndProperties();
+					GUI::Properties::EndProperties();
 
 					ImGui::TreePop();
 				}
@@ -363,6 +454,75 @@ namespace SW {
 
 			std::ofstream fout(path);
 			fout << output.c_str();
+		}
+	}
+
+	void SpritesheetEditor::GenerateFromGridPopup(bool* opened)
+	{
+		if (*opened)
+			ImGui::OpenPopup(SW_ICON_GRID " Generate sprites from a grid");
+
+		if (ImGui::BeginPopupModal(SW_ICON_GRID " Generate sprites from a grid", nullptr)) {
+			ImGui::Text("Use this tool to generate sprites from a grid.");
+			ImGui::Separator();
+
+			const f32 currentGridSize = (*m_Spritesheet)->GridSize;
+
+			GUI::Properties::BeginProperties("##spritesheet_editor_generate_from_grid_options");
+
+			const Texture2D* texture = (*m_Spritesheet)->GetSpritesheetTexture();
+			const f32 texWidth = (f32)texture->GetWidth();
+			const f32 texHeight = (f32)texture->GetHeight();
+
+			static glm::vec2 gridSize = { (*m_Spritesheet)->GridSize, (*m_Spritesheet)->GridSize };
+			GUI::Properties::Vector2InputProperty(&gridSize, "Grid Size", 
+				"Grid size used to generate sprites, can be different from the current grid size.", currentGridSize, MIN_GRID_SIZE, MAX_GRID_SIZE, "%1.f");
+
+			static glm::vec2 textureOffset = { 0.f, 0.f };
+			GUI::Properties::Vector2InputProperty(&textureOffset, "Texture Offset", 
+				"Offset in texture, used to start cutting sprites from a different position.", 0.f, 0.f, FLT_MAX, "%1.f");
+
+			static f32 cutSizeX = (f32)texture->GetWidth();
+			GUI::Properties::ScalarInputProperty(&cutSizeX, "Cut Size X", 
+				"Cut size in X axis, part or whole spritesheet texture.", 16.f, 32.f, 0.f, (f32)texture->GetWidth(), "%1.f");
+
+			static f32 cutSizeY = (f32)texture->GetHeight();
+			GUI::Properties::ScalarInputProperty(&cutSizeY, "Cut Size Y", 
+				"Cut size in Y axis, part or whole spritesheet texture.", 16.f, 32.f, 0.f, (f32)texture->GetHeight(), "%1.f");
+
+			GUI::Properties::EndProperties();
+
+			if (ImGui::Button("Generate", ImVec2(120, 0))) {
+				*opened = false;
+
+				const glm::vec2 offset = (*m_Spritesheet)->CenterOffset;
+
+				for (f32 x = offset.x + textureOffset.x; x < offset.x + cutSizeX + textureOffset.x; x += gridSize.x) {
+					for (f32 y = offset.y + textureOffset.y; y < offset.y + cutSizeY + textureOffset.y; y += gridSize.y) {
+						SpriteData data(Random::CreateTag());
+						data.Position = { x, y };
+						data.Size = gridSize;
+
+						(*m_Spritesheet)->Sprites.emplace_back(data);
+					}
+				}
+
+				(*m_Spritesheet)->GridSize = gridSize.x;
+
+				ImGui::CloseCurrentPopup();
+			}
+
+			ImGui::SetItemDefaultFocus();
+
+			ImGui::SameLine();
+
+			if (ImGui::Button("Cancel", ImVec2(120, 0))) {
+				*opened = false;
+
+				ImGui::CloseCurrentPopup();
+			}
+
+			ImGui::EndPopup();
 		}
 	}
 
--- EngineEditor/src/AssetPanels/SpritesheetEditor.hpp
+++ EngineEditor/src/AssetPanels/SpritesheetEditor.hpp
@@ -1,8 +1,8 @@
 /**
  * @file SpritesheetEditor.hpp
  * @author Tycjan Fortuna (242213@edu.p.lodz.pl)
- * @version 0.1.0
- * @date 2024-03-17
+ * @version 0.2.0
+ * @date 2024-05-02
  *
  * @copyright Copyright (c) 2024 Tycjan Fortuna
  */
@@ -39,16 +39,20 @@ namespace SW {
 		ImGuiEx::Canvas m_Canvas;
 
 		Spritesheet** m_Spritesheet = nullptr;
+		std::string m_Name;
 		
 		bool m_IsCanvasHovered = false;
 
 		std::string m_SearchString;		/**< The search string used to filter entities in the scene hierarchy. */
+
+		bool m_OpenGenerateFromGridModal = false;
 
 	private:
 		void DrawRectOnCanvas(ImDrawList* drawList, const glm::vec2& position, const glm::vec2& size, u32 color) const;
 		void RenderSpriteCards(f32 vscale);
 		void AddNewSprite();
 		void ExportSprites() const;
+		void GenerateFromGridPopup(bool* opened);
 	};
 
 }
